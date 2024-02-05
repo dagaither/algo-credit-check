0ddf3e6e34be5dfc69c05d7bbf68ee47fd5845b8
David Gaither <gaither@protonmail.com>
2024-02-05 12:37:25 -0500

    Python complete

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,22 @@
+singles = []
+
 def credit_check(str):
-    pass
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+    for num in str[-1::-2]:
+        singles.append(int(num))
 
+    for num in str[-2::-2]:
+        num = int(num) * 2
+
+        if num < 9:
+            singles.append(num)
+        else:
+            num = ((num //10) + (num %10))
+            singles.append(num)
+
+    if (sum(singles)) % 10 == 0:
+        return "The number is valid!"
+    
+    else:
+        return "The number is invalid!"
+        
