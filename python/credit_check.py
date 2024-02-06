577652081ed882d38b2f75918f8492d0648e9683
David Gaither <gaither@protonmail.com>
2024-02-05 23:46:05 -0500

    Pytest complete

--- python/credit_check.py
+++ python/credit_check.py
@@ -19,4 +19,10 @@ def credit_check(str):
     
     else:
         return "The number is invalid!"
-        
+
+# print(credit_check("4024007136512380"))
+# print(credit_check("6011797668867828"))
+
+# print(credit_check("4024007106512380"))    
+# print(credit_check("6011797668868728"))
+# print(credit_check("6011797668868728") == "The number is invalid!")
--- python/test_credit_check.py
+++ python/test_credit_check.py
@@ -1,11 +1,17 @@
-# Can you translate this driver code to unit tests?
-
 from credit_check import credit_check
+import pytest
 
-print(credit_check('5541808923795240') == "The number is valid!")
-print(credit_check("4024007136512380") == "The number is valid!")
-print(credit_check("6011797668867828") == "The number is valid!")
+def test_one_pass():
+    assert credit_check('5541808923795240') == "The number is valid!"
+def test_two_pass():
+    assert credit_check('5541808923795240') == "The number is valid!"
+def test_three_pass():
+    assert credit_check("4024007136512380") == "The number is valid!"
+def test_four_pass():
+    assert credit_check("6011797668867828") == "The number is valid!"
 
-print(credit_check("5541801923795240") == "The number is invalid!")
-print(credit_check("4024007106512380") == "The number is invalid!")
-print(credit_check("6011797668868728") == "The number is invalid!")
+
+def test_one_fail():
+    assert credit_check("5541801923795240") == "The number is invalid!"
+def test_two_fail():
+    assert credit_check("4024007106512380") == "The number is invalid!"
